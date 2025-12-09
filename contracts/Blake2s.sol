157770f69f5278d3aa293909d9b4c07e34fee8d1
Francisco Giordano <fg@frang.io>
2025-12-09 16:16:56 -0300

    sigma

--- contracts/Blake2s.sol
+++ contracts/Blake2s.sol
@@ -191,28 +191,30 @@ library Blake2s {
         }
     }
 
-    function round(uint32[16] memory v, uint32[16] memory m, uint256 r) private pure {
-        uint8[16][10] memory sigma = [
-            [0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15],
-            [14, 10,  4,  8,  9, 15, 13,  6,  1, 12,  0,  2, 11,  7,  5,  3],
-            [11,  8, 12,  0,  5,  2, 15, 13, 10, 14,  3,  6,  7,  1,  9,  4],
-            [7,  9,  3,  1, 13, 12, 11, 14,  2,  6,  5, 10,  4,  0, 15,  8],
-            [9,  0,  5,  7,  2,  4, 10, 15, 14,  1, 11, 12,  6,  8,  3, 13],
-            [2, 12,  6, 10,  0, 11,  8,  3,  4, 13,  7,  5, 15, 14,  1,  9],
-            [12,  5,  1, 15, 14, 13,  4, 10,  0,  7,  6,  3,  9,  2,  8, 11],
-            [13, 11,  7, 14, 12,  1,  3,  9,  5,  0, 15,  4,  8,  6,  2, 10],
-            [6, 15, 14,  9, 11,  3,  0,  8, 12,  2, 13,  7,  1,  4, 10,  5],
-            [10,  2,  8,  4,  7,  6,  1,  5, 15, 11,  9, 14,  3, 12, 13,  0]
-        ];
+    uint256 constant SIGMA0123 = 0x8F04A562EBCD1397491763EADF250C8B357B20C16DF984AEFEDCBA9876543210;
+    uint256 constant SIGMA4567 = 0xA2684F05931CE7BDB8293670A4DEF15C91EF57D438B0A6C2D386CB1EFA427509;
+    uint256 constant SIGMA89 = 0x0DC3E9BF5167482A5A417D2C803B9EF6;
 
-        G(v, m, 0, 4,  8, 12, m[sigma[r][0]],  m[sigma[r][1]]);
-        G(v, m, 1, 5,  9, 13, m[sigma[r][2]],  m[sigma[r][3]]);
-        G(v, m, 2, 6, 10, 14, m[sigma[r][4]],  m[sigma[r][5]]);
-        G(v, m, 3, 7, 11, 15, m[sigma[r][6]],  m[sigma[r][7]]);
-        G(v, m, 0, 5, 10, 15, m[sigma[r][8]],  m[sigma[r][9]]);
-        G(v, m, 1, 6, 11, 12, m[sigma[r][10]], m[sigma[r][11]]);
-        G(v, m, 2, 7,  8, 13, m[sigma[r][12]], m[sigma[r][13]]);
-        G(v, m, 3, 4,  9, 14, m[sigma[r][14]], m[sigma[r][15]]);
+    function sigma(uint256 r, uint256 i) private pure returns (uint256) {
+        unchecked {
+            uint256 packed;
+            if (r < 4) packed = SIGMA0123;
+            else if (r < 8) packed = SIGMA4567;
+            else packed = SIGMA89;
+
+            return (packed >> (((r & 0x3) << 6) + (i << 2))) & 0xF;
+        }
+    }
+
+    function round(uint32[16] memory v, uint32[16] memory m, uint256 r) private pure {
+        G(v, m, 0, 4,  8, 12, m[sigma(r, 0)],  m[sigma(r, 1)]);
+        G(v, m, 1, 5,  9, 13, m[sigma(r, 2)],  m[sigma(r, 3)]);
+        G(v, m, 2, 6, 10, 14, m[sigma(r, 4)],  m[sigma(r, 5)]);
+        G(v, m, 3, 7, 11, 15, m[sigma(r, 6)],  m[sigma(r, 7)]);
+        G(v, m, 0, 5, 10, 15, m[sigma(r, 8)],  m[sigma(r, 9)]);
+        G(v, m, 1, 6, 11, 12, m[sigma(r, 10)], m[sigma(r, 11)]);
+        G(v, m, 2, 7,  8, 13, m[sigma(r, 12)], m[sigma(r, 13)]);
+        G(v, m, 3, 4,  9, 14, m[sigma(r, 14)], m[sigma(r, 15)]);
     }
 }
 
